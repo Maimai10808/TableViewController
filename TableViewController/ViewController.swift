978a800c3ddc9be18a6129fc0fc89847aca89ca4
Maimai10808 <331686549@qq.com>
2025-06-07 10:00:00 +0800

    didSelectRowAt

--- TableViewController/ViewController.swift
+++ TableViewController/ViewController.swift
@@ -8,41 +8,41 @@
 import UIKit
 
 
-struct User {
-    let name: String
-    let shoppingItems : [ShoppingItem]
-}
+//struct User {
+//    let name: String
+//    let shoppingItems : [ShoppingItem]
+//
 
 struct ShoppingItem {
     let title: String
     let subTitle: String
-    let image: UIImage
+    var isSelected = false
 }
 
 
 class ViewController: UITableViewController {
     
-    var users: [User] = [
-        User(name: "Mai", shoppingItems: [ShoppingItem(title: "Apple", subTitle: "Buy 4 apples", image: UIImage(systemName: "applelogo")!),
-                                          ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas", image: UIImage(systemName: "leaf")!)]),
-        User(name: "Paul", shoppingItems: [ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges", image: UIImage(systemName: "circle.fill")!), // 可用其他更合适图标替代
-                                           ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries", image: UIImage(systemName: "heart.fill")!),
-                                           ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon", image: UIImage(systemName: "drop.fill")!)  ]),
-        User(name: "Laura", shoppingItems: [ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges", image: UIImage(systemName: "circle.fill")!), // 可用其他更合适图标替代
-                                           ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries", image: UIImage(systemName: "heart.fill")!),
-                                           ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon", image: UIImage(systemName: "drop.fill")!)  ]),
-        User(name: "Jason", shoppingItems: [ShoppingItem(title: "Apple", subTitle: "Buy 4 apples", image: UIImage(systemName: "applelogo")!),
-                                          ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas", image: UIImage(systemName: "leaf")!)])
-        ]
+//    var users: [User] = [
+//        User(name: "Mai", shoppingItems: [ShoppingItem(title: "Apple", subTitle: "Buy 4 apples", image: UIImage(systemName: "applelogo")!),
+//                                          ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas", image: UIImage(systemName: "leaf")!)]),
+//        User(name: "Paul", shoppingItems: [ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges", image: UIImage(systemName: "circle.fill")!), // 可用其他更合适图标替代
+//                                           ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries", image: UIImage(systemName: "heart.fill")!),
+//                                           ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon", image: UIImage(systemName: "drop.fill")!)  ]),
+//        User(name: "Laura", shoppingItems: [ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges", image: UIImage(systemName: "circle.fill")!), // 可用其他更合适图标替代
+//                                           ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries", image: UIImage(systemName: "heart.fill")!),
+//                                           ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon", image: UIImage(systemName: "drop.fill")!)  ]),
+//        User(name: "Jason", shoppingItems: [ShoppingItem(title: "Apple", subTitle: "Buy 4 apples", image: UIImage(systemName: "applelogo")!),
+//                                          ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas", image: UIImage(systemName: "leaf")!)])
+//        ]
        
     
-//    var shoppingItems: [ShoppingItem] = [
-//        ShoppingItem(title: "Apple", subTitle: "Buy 4 apples", image: UIImage(systemName: "applelogo")!),
-//        ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas", image: UIImage(systemName: "leaf")!),
-//        ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges", image: UIImage(systemName: "circle.fill")!), // 可用其他更合适图标替代
-//        ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries", image: UIImage(systemName: "heart.fill")!),
-//        ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon", image: UIImage(systemName: "drop.fill")!) // 近似替代
-//    ]
+    var shoppingItems: [ShoppingItem] = [
+        ShoppingItem(title: "Apple", subTitle: "Buy 4 apples"),
+        ShoppingItem(title: "Banana", subTitle: "Get a bunch of bananas"),
+        ShoppingItem(title: "Orange", subTitle: "Buy 3 oranges"), // 可用其他更合适图标替代
+        ShoppingItem(title: "Strawberry", subTitle: "Buy 1 box of strawberries"),
+        ShoppingItem(title: "Watermelon", subTitle: "Buy a watermelon") // 近似替代
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,40 +50,48 @@ class ViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return users.count
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users[section].shoppingItems.count
+        return shoppingItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        let  cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingSubitleCell", for: indexPath)
-        let shoppingItem = users[indexPath.section].shoppingItems[indexPath.row]
+        let shoppingItem = shoppingItems[indexPath.row]
         
         if #available(iOS 14, *) {
             var content = cell.defaultContentConfiguration()
             
             content.text          =  shoppingItem.title
             content.secondaryText =  shoppingItem.subTitle
-            content.image         =  shoppingItem.image
+            content.image         =  shoppingItem.isSelected ? UIImage(systemName: "star.fill")! : UIImage(systemName: "star")!
             
             cell.contentConfiguration = content
         } else {
             
             cell.textLabel?.text       = shoppingItem.title
             cell.detailTextLabel?.text = shoppingItem.subTitle
-            cell.imageView?.image      = shoppingItem.image
+            cell.imageView?.image      = shoppingItem.isSelected ? UIImage(systemName: "star.fill")! : UIImage(systemName: "star")!
+            
         }
            return cell
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return users[section].name
-    }
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return users[section].name
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+//        return "\(users[section].name) has \(users[section].shoppingItems.count) items"
+//    }
     
-    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return "\(users[section].name) has \(users[section].shoppingItems.count) items"
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did tap row")
+        shoppingItems[indexPath.row].isSelected.toggle()
+//        shoppingItems[indexPath.row].isSelected = !shoppingItems[indexPath.row].isSelected
+        tableView.reloadData()
     }
 
 
